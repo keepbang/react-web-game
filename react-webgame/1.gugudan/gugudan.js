8786147acd87be3380f7396f529e95c08a70a189
keepbang <rlqud1125@gmail.com>
2020-02-10 23:31:58 +0900

    gugudan

--- react-webgame/1.gugudan/gugudan.js
+++ react-webgame/1.gugudan/gugudan.js
@@ -5,39 +5,59 @@ class GuGuDan extends React.Component{
         second : Math.ceil(Math.random() * 9),
         value : '',//입력창
         result : '',//결과값
+        count : 0,//정답횟수
     }
-    
     
     onSubmit = (e) => {
         e.preventDefault();
         let resultTmp = this.state.first * this.state.second;
         if(parseInt(this.state.value) === resultTmp){
-            this.setState({
-                result : this.state.value + " 정답!",
-                first : Math.ceil(Math.random() * 9),
-                second : Math.ceil(Math.random() * 9),
-                value : ''
-            })
-            
+            this.setState((prevState) => {
+                return {
+                    result : "정답 : " + prevState.value,
+                    first : Math.ceil(Math.random() * 9),
+                    second : Math.ceil(Math.random() * 9),
+                    value : ''
+                };
+            });
+
+            this.setState((prevState) => {
+                return{
+                    count: prevState.count + 1,
+                }
+            });
+
+           this.input.focus();
         }else{
             this.setState({
                 result : "땡!",
                 value : ''
-            })
+            });
+            this.input.focus();
         }
     }
 
+    onChange = (e) =>{
+        this.setState({value : e.target.value})
+    }
+
+    inputFocus = (c) => {this.input = c;}
+
+    input;
+
     render(){
+        console.log("render!");
         return(
-            <fragment>
+            <React.Fragment>
                 <div>{this.state.first} 곱하기 {this.state.second}는?</div>
                 <form onSubmit={this.onSubmit}>
-                    <input type="number" value={this.state.value} 
-                        onChange = {(e) => this.setState({value : e.target.value})}/>
+                    <input ref={this.inputFocus} type="number" value={this.state.value} 
+                        onChange = {this.onChange}/>
                     <button>입력!</button>
                 </form>
                 <div>{this.state.result}</div>
-            </fragment>
+                <div>정답 횟수 : {this.state.count}</div>
+            </React.Fragment>
        );
         
     }
